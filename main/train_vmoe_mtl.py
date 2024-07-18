6099dc753bc5c63fa181b37f7208ee92e987bac1
Chen Kefan <1103884021@qq.com>
2024-07-18 11:47:03 +0800

    m config.py

--- main/train_vmoe_mtl.py
+++ main/train_vmoe_mtl.py
@@ -43,7 +43,7 @@ def main(_):
   config_params = _CONFIG_PARAM.value or []
   # enable relative paths within p5x configs.
   # gin.add_config_file_search_path('third_party/py/t5x/configs')
-  base_dir = os.path.join('home','yhy','code','ckf','adamv_moe')
+  base_dir = os.path.join('','home','yhy','code','ckf','adamv_moe')
   gin.add_config_file_search_path(os.path.join(base_dir,'gin_configs'))
   gin.parse_config_files_and_bindings([_CONFIG_PATH.value], config_params)
   if not _AES.value:
