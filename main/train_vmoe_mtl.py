b98178e6c6caa1c5c82dbfaea6966de6547453f4
Chen Kefan <1103884021@qq.com>
2024-07-18 11:15:12 +0800

    m config.py

--- main/train_vmoe_mtl.py
+++ main/train_vmoe_mtl.py
@@ -41,7 +41,7 @@ def main(_):
   config_params = _CONFIG_PARAM.value or []
   # enable relative paths within p5x configs.
   # gin.add_config_file_search_path('third_party/py/t5x/configs')
-  gin.add_config_file_search_path('/home/yhy/code/ckfadamv_moe/ex_repo/vision_transformer/flaxformer/flaxformer/flaxformer/architectures/fido/example_configs/')
+  gin.add_config_file_search_path('/home/yhy/code/ckf/adamv_moe/ex_repos/flaxformer/flaxformer/t5x/configs')
   gin.parse_config_files_and_bindings([_CONFIG_PATH.value], config_params)
   if not _AES.value:
     trainer_utils.train_vmoe_mtl(_OUTPUT_DIR.value)
